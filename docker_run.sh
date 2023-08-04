c9ab0035a029d0f23a8eb6d0a7e85f6ed1f603f1
Niccolò Maggioni <nicco.maggioni@gmail.com>
2023-08-04 12:00:47 +0200

    Update Docker wrapper
    Match user permissions, remove old X11 auth

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#docker run --rm -it -v "$(pwd):/docs" -u=$(id -u $USER):$(id -g $USER) -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw python-pathfinder-tools $@
-docker run --rm -it -v "$(pwd):/docs" -e DISPLAY --net=host -v $XAUTHORITY:/root/.Xauthority -v /tmp/.X11-unix:/tmp/.X11-unix python-pathfinder-tools $@
+docker run --rm -it -v "$(pwd):/docs" -u=$(id -u $USER):$(id -g $USER) -e DISPLAY --net=host -v $XAUTHORITY:/root/.Xauthority -v /tmp/.X11-unix:/tmp/.X11-unix python-pathfinder-tools $@
